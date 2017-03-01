a2c45d53ee11a0a293641ed247a69fdb08ed0b4b
Milovan Minic <milovanminic@Donalds.local>
2017-03-01 21:40:50 +0100

    Added array of countries and function that shows flags for question

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -14,9 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
