b8771d8714e2931236e675380fa31be6050e02c1
Milovan Minic <milovan.minic@gmail.com>
2017-03-02 14:39:44 +0100

    Connected all three buttons with storyboard

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -54,6 +54,8 @@ class ViewController: UIViewController {
         title = countries[correctAnswer].uppercased()
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
