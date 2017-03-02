317961aaf5736fd9561afd4227ac6bf93f6dc77b
Milovan Minic <milovan.minic@gmail.com>
2017-03-02 14:13:49 +0100

    Made possible to have correct answer to the question

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -48,6 +48,8 @@ class ViewController: UIViewController {
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
     }
 
     override func didReceiveMemoryWarning() {
